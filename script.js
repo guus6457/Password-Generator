92a96fb6c7f91a94e7db61ac176b9525e3e72468
Guus <57826942+guus6457@users.noreply.github.com>
2024-04-28 22:10:00 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,98 +1,99 @@
 // Function to generate a random password
+
 function generatePassword() {
-    var length = document.getElementById("length").value;
-    var includeNumbers = document.getElementById("numbers").checked;
-    var includeLowercase = document.getElementById("lowercase").checked;
-    var includeUppercase = document.getElementById("uppercase").checked;
-    var includeSymbols = document.getElementById("symbols").checked;
+  var length = document.getElementById("length").value;
+  var includeNumbers = document.getElementById("numbers").checked;
+  var includeLowercase = document.getElementById("lowercase").checked;
+  var includeUppercase = document.getElementById("uppercase").checked;
+  var includeSymbols = document.getElementById("symbols").checked;
 
-    // Check if any option is selected
-    if (!includeNumbers && !includeLowercase && !includeUppercase && !includeSymbols) {
-        // Display warning message
-        showModal();
-        return; // Exit function
-    }
+  // Check if any option is selected
+  if (!includeNumbers && !includeLowercase && !includeUppercase && !includeSymbols) {
+    // Display warning message
+    showModal();
+    return; // Exit function
+  }
 
-    var charset = "";
-    if (includeNumbers) charset += "0123456789";
-    if (includeLowercase) charset += "abcdefghijklmnopqrstuvwxyz";
-    if (includeUppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    if (includeSymbols) charset += "!@#$%^&*()-_=+[{]}|;:,<.>/?";
+  var charset = "";
+  if (includeNumbers) charset += "0123456789";
+  if (includeLowercase) charset += "abcdefghijklmnopqrstuvwxyz";
+  if (includeUppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (includeSymbols) charset += "!@#$%^&*()-_=+[{]}|;:,<.>/?";
 
-    var password = "";
-    for (var i = 0; i < length; i++) {
-        var randomIndex = Math.floor(Math.random() * charset.length);
-        password += charset[randomIndex];
-    }
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
 
-    // Display the generated password
-    document.getElementById("password").innerText = password;
+  // Display the generated password
+  document.getElementById("password").innerText = password;
 }
 
 // Function to download the generated password
 function downloadPassword() {
-    var password = document.getElementById("password").innerText;
-    if (password) {
-        var element = document.createElement('a');
-        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(password));
-        element.setAttribute('download', 'generated_password.txt');
-        element.style.display = 'none';
-        document.body.appendChild(element);
-        element.click();
-        document.body.removeChild(element);
-    } else {
-        showModal();
-    }
+  var password = document.getElementById("password").innerText;
+  if (password) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(password));
+    element.setAttribute('download', 'generated_password.txt');
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  } else {
+    showModal();
+  }
 }
 
 // Function to display the modal
 function showModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "block";
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
 }
 
 // Function to close the modal
 function closeModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none";
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
 }
 
 // Function to enable dark mode
 function enableDarkMode() {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
+  document.documentElement.setAttribute('data-theme', 'dark');
+  localStorage.setItem('theme', 'dark');
 }
 
 // Function to disable dark mode
 function disableDarkMode() {
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light');
+  document.documentElement.setAttribute('data-theme', 'light');
+  localStorage.setItem('theme', 'light');
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    const switchInput = document.querySelector('.switch input');
+  const switchInput = document.querySelector('.switch input');
 
-    switchInput.addEventListener('change', function() {
-        if (this.checked) {
-            enableDarkMode();
-        } else {
-            disableDarkMode();
-        }
-    });
-
-    // Check local storage for theme preference
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark') {
-        enableDarkMode();
-        switchInput.checked = true;
+  switchInput.addEventListener('change', function() {
+    if (this.checked) {
+      enableDarkMode();
+    } else {
+      disableDarkMode();
     }
+  });
+
+  // Check local storage for theme preference
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    enableDarkMode();
+    switchInput.checked = true;
+  }
 });
 
 function toggleInfoVisibility() {
-    var infoButton = document.getElementById("info-button");
-    var infoSection = document.getElementById("info-section");
+  var infoButton = document.getElementById("info-button");
+  var infoSection = document.getElementById("info-section");
 
-    // Toggle active class to move the button and reveal the text
-    infoButton.classList.toggle("active");
-    infoSection.classList.toggle("active");
+  // Toggle active class to move the button and reveal the text
+  infoButton.classList.toggle("active");
+  infoSection.classList.toggle("active");
 }
